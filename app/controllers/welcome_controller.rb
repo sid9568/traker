edf391e6e9dfe3ac336e527d8145ab392c6857af
sid9568 <sid20319@gmail.com>
2023-07-14 17:04:51 +0530

    better

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   end
+ def download_bag_sh
+  file_path = Rails.root.join('public', './test2.sh')
+  if File.exist?(file_path)
+    send_file(file_path, filename: './test2.sh', disposition: 'attachment')
+  else
+    flash[:error] = 'File not found.'
+    redirect_to root_path
+  end
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'download_sid_sh', to: 'welcome#download_bag_sh'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
